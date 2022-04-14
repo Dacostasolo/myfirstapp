154a033f72e678cc41a6200a8c3bfe422b3a41f0
Dacosta <dacsolo10@gmail.com>
2022-04-14 20:03:03 +0000

    minor changes

--- src/components/FeedBackForm.jsx
+++ src/components/FeedBackForm.jsx
@@ -4,29 +4,40 @@ import Card from './shared/Card'
 
 function FeedBackForm() {
   const [text, setTextState] = useState('')
-  const [message, setMState] = useState('')
-  const [button, setButtonState] = useState(false)
+  const [message, setMessageState] = useState('')
+  const [isDisabled, setButtonState] = useState(true)
 
   const textChangeHanler = (e) => {
-    setTextState(() => {
-      return e.target.value
-    })
+    if (text === '') {
+      setMessageState(null)
+      setButtonState(true)
+    } else if (text !== '' && text.trim().length < 10) {
+      setMessageState('Text should be at least 10 characters')
+      setButtonState(true)
+    } else {
+      setButtonState(false)
+      setMessageState(null)
+    }
+    setTextState(e.target.value)
   }
 
   return (
     <Card>
-      <h2>How will you rate your service with us?</h2>
-      <div className='input-group'>
-        <input
-          type='text'
-          placeholder='write a review'
-          onChange={textChangeHanler}
-          value={text}
-        />
-        <Button type='submit' isDisabled={button}>
-          <span></span>
-        </Button>
-      </div>
+      <form>
+        <h2>How will you rate your service with us?</h2>
+        <div className='input-group'>
+          <input
+            type='text'
+            placeholder='write a review'
+            onChange={textChangeHanler}
+            value={text}
+          />
+          <Button type='submit' isDisabled={isDisabled}>
+            <span>Send</span>
+          </Button>
+        </div>
+        {message && <div className='message'>{message}</div>}
+      </form>
     </Card>
   )
 }
